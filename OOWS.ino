c51430e98f01eec51aabd33ebc46e3b7c1b4155e
Daniele <moro.daniele@gmail.com>
2015-07-10 21:24:58 +0000

    Updated Thermistor Class

--- OOWS.ino
+++ OOWS.ino
@@ -2,6 +2,14 @@
 #include <DHT.h>
 #include <WiFi.h>
 
+#define THERM_A_COEFFICIENT 0.003354015
+#define THERM_B_COEFFICIENT 0.000256277
+#define THERM_C_COEFFICIENT 0.000002082921
+#define THERM_D_COEFFICIENT 0.000000073003206
+#define THERM_PIN 2
+#define AREF_VOLTAGE 5
+#define THERM_RESISTOR 10000
+
 WiFiClient client;
 
 class Sensor {
@@ -9,6 +17,15 @@ public:
   int virtual getNumberOfValues() = 0;
   float virtual getValue(int num) = 0;
   String virtual getValueName(int num) = 0;
+  
+  float readVoltage(int pin) {
+    /*
+    analogRead reads a value from the sensor, but scales it from 0-1023
+    It instead should be from 0 volts to the constant AREF_VOLTAGE (maximum number of volts)
+    To undo the scaling, we use a ratio
+    */
+    return analogRead(pin) * AREF_VOLTAGE / 1023.0;
+  }
 };
 
 class DHT22Sensor : public Sensor {
@@ -46,17 +63,36 @@ public:
   }
 };
 
-/*
-WIP
+
 class ThermistorSensor : public Sensor {
+protected:
+  int pin;
+
 public:
-  TermistorSensor(int pin) {}
+  ThermistorSensor(int pin) {
+    this->pin = pin;
+  }
 
   int getNumberOfValues() {
     return 1;
   }
+  
+  float getValue(int num){
+    float Vout = readVoltage(pin);
+    float rt = (THERM_RESISTOR*(Vout/AREF_VOLTAGE))/(1-(Vout/AREF_VOLTAGE));
+    float ln = log(rt/THERM_RESISTOR);
+    float tempK = pow(THERM_A_COEFFICIENT+THERM_B_COEFFICIENT*ln+THERM_C_COEFFICIENT*(pow(ln, 2))+THERM_D_COEFFICIENT*(pow(ln,3)),(-1));
+    float thermTemperature = tempK-273.5;
+    return thermTemperature;
+  }
+  
+  
+  String getValueName(int num) {
+    return "Temperature";
+  }
+  
 };
-*/
+
 
 class Output {
 public:
@@ -90,8 +126,8 @@ public:
 
 void setup() {
   while (WiFi.begin("Bronco-Guest") != WL_CONNECTED);
-  Sensor* sensors[] = {new DHT22Sensor(2)};
-  int sensorsLength = 1;
+  Sensor* sensors[] = {new DHT22Sensor(2), new ThermistorSensor(1)};
+  int sensorsLength = 2;
   Output* outputs[] = {new ThingspeakOutput()};
   int outputsLength = 1;
   for (int i = 0; i < sensorsLength; i++) {
@@ -109,4 +145,3 @@ void setup() {
 void loop() {
   
 }
-
