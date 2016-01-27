2655d2b685305c5b0653b60aec2e08bfdb804f9e
Lee Bousfield <ljbousfield@gmail.com>
2016-01-26 17:02:38 -0700

    Fixed config example

--- config.example.h
+++ config.example.h
@@ -1,7 +1,6 @@
-
 #define ARDUINO_NAME String("Name")
 #define NETWORK "Network_Name"
 #define APIKEY String("0123456789ABCDEF")
-#define CUSTOM_DATA_SERVER_HOST String("12.34.56.78")
+#define CUSTOM_DATA_SERVER_IP cc3000.IP2U32(12, 34, 12, 34)
 #define SENSORS {new DHT22Sensor(2)}
 #define OUTPUTS {new SerialOutput()}
