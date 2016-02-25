ded8d2e6abe0d452bda0bb187f787bf8c2f56bfe
Lee Bousfield <ljbousfield@gmail.com>
2016-02-25 15:51:06 -0700

    Disable WiFi in example config

--- config.example.h
+++ config.example.h
@@ -1,8 +1,7 @@
 #define ARDUINO_NAME String("Name")
-#define CC3000_SHIELD
 #define NETWORK "Network_Name"
 #define APIKEY String("0123456789ABCDEF")
-#define CUSTOM_DATA_SERVER_IP cc3000.IP2U32(12, 34, 12, 34)
+#define CUSTOM_DATA_SERVER_IP 0
 #define CUSTOM_DATA_SERVER_PASSWORD String("1234")
 #define SENSORS {new DHT22Sensor(2)}
 #define SENSORS_LENGTH 1
